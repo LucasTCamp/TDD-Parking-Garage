3f77d347261f2e5c9e881527393cac2dce7a77c7
LucasTCamp <lucastcampbell@outlook.com>
2026-04-23 08:44:56 -0400

    fixed another syntax error for get_available spots

--- garage.py
+++ garage.py
@@ -14,7 +14,7 @@ def exit_garage(garage, car_id):
 
 
 def get_available_spots(garage):
-    return garage["capacity"] - len(garage["cars"]))
+    return garage["capacity"] - len(garage["cars"])
 
 def calculate_fee(hours, rate):
     pass

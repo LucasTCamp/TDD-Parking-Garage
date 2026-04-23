bd8d07f688a1e50187ea035278df1892eaaad6c3
LucasTCamp <lucastcampbell@outlook.com>
2026-04-23 08:16:17 -0400

    test enter garage raises ValueError if the garage is full

--- test_garage.py
+++ test_garage.py
@@ -7,3 +7,12 @@ def test_enter_garage():
     }
     enter_garage(garageDict, "Bugatti7", 13)
     assert "Bugatti7" in garageDict["cars"].keys()
+
+
+def test_enter_garage_valueError_if_full():
+    with pytest.raises(ValueError):
+        garageDict = {
+        "capacity": 1,   # total number of spots
+        "cars": {"Rat67": 15}         # car_id -> entry_hour (int)
+        }
+        enter_garage(garageDict, "Bugatti7", 13)
